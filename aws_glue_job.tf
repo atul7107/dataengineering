768ab146005713c2978bbea7d912676a43a87216
Atul Yadav <atul.yadav7717@gmail.com>
2023-04-02 20:53:32 +0530

    Update aws_glue_job.tf

--- aws_glue_job.tf
+++ aws_glue_job.tf
@@ -40,7 +40,7 @@ resource "aws_s3_bucket_object" "scripts_raw_to_prepared" {
   bucket     = module.dl_s3_internal.s3_bucket_id
   key        = "${var.dl_glue_job_raw_to_prepared.scripts_folder}/${var.dl_glue_job_raw_to_prepared.name}.py"
   content    = file("scripts/glue_jobs/${var.dl_glue_job_raw_to_prepared.name}.py")
-  kms_key_id = module.dl_kms.kms_arn
+  #kms_key_id = module.dl_kms.kms_arn
 
 }
     
