55b2a24e94abc42d73323782797a3f9a53f498b8
Atul Yadav <atul.yadav7717@gmail.com>
2023-04-02 12:10:42 +0530

    Update main.tf
    File added for Data engineering pipelines

---
+++ main.tf
@@ -0,0 +1,69 @@
+# Require TF version to be same as or greater than 0.12.13
+terraform {
+  required_version = ">=0.12.13"
+  #backend "s3" {
+  #  bucket         = "kyler-github-actions-demo-terraform-tfstate"
+  #  key            = "terraform.tfstate"
+  #  region         = "us-east-1"
+  #  dynamodb_table = "aws-locks"
+  #  encrypt        = true
+  #}
+}
+
+# Download any stable version in AWS provider of 2.36.0 or higher in 2.36 train
+provider "aws" {
+  region  = "us-east-1"
+  version = "~> 2.36.0"
+}
+
+module "dl_s3_raw" {
+  source = "./modules/aws-s3-bucket"
+
+  block_public_acls       = var.dl_s3_raw.block_public_acls
+  block_public_policy     = var.dl_s3_raw.block_public_policy
+  bucket_name             = var.dl_s3_raw.bucket_name
+  create_s3_bucket        = var.dl_s3_raw.create_s3_bucket
+  force_destroy           = var.dl_s3_raw.force_destroy
+  kms_master_key_arn      = module.dl_kms.kms_arn
+  lifecycle_rule          = var.dl_s3_raw.lifecycle_rule
+  restrict_public_buckets = var.dl_s3_raw.restrict_public_buckets
+  sse_algorithm           = var.dl_s3_raw.sse_algorithm
+  sse_prevent             = var.dl_s3_raw.sse_prevent
+  versioning              = var.dl_s3_raw.versioning
+  tags                    = var.tags
+
+  depends_on = [module.dl_kms]
+}
+
+dl_s3_raw = {
+  block_public_acls       = true
+  block_public_policy     = true
+  bucket_name             = "raw"
+  create_s3_bucket        = true
+  force_destroy           = true
+  restrict_public_buckets = true
+  sse_algorithm           = "aws:kms"
+  sse_prevent             = true
+  versioning              = true
+  lifecycle_rule = [{
+    id      = "dl-s3-raw-transition"
+    enabled = true
+
+    transition = [
+      {
+        days          = 30
+        storage_class = "STANDARD_IA"
+      },
+      {
+        days          = 60
+        storage_class = "GLACIER"
+      }
+    ]
+
+    expiration = [
+      {
+        days = 180
+      }
+    ]
+  }]
+}
