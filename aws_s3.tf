c72be8925d442dbae48e8bbf375e190a19a1ca80
Atul Yadav <atul.yadav7717@gmail.com>
2023-04-02 13:26:14 +0530

    Update aws_s3.tf

--- aws_s3.tf
+++ aws_s3.tf
@@ -34,36 +34,3 @@ module "dl_s3_raw" {
 
   depends_on = [module.dl_kms]
 }
-
-dl_s3_raw = {
-  block_public_acls       = true
-  block_public_policy     = true
-  bucket_name             = "raw"
-  create_s3_bucket        = true
-  force_destroy           = true
-  restrict_public_buckets = true
-  sse_algorithm           = "aws:kms"
-  sse_prevent             = true
-  versioning              = true
-  lifecycle_rule = [{
-    id      = "dl-s3-raw-transition"
-    enabled = true
-
-    transition = [
-      {
-        days          = 30
-        storage_class = "STANDARD_IA"
-      },
-      {
-        days          = 60
-        storage_class = "GLACIER"
-      }
-    ]
-
-    expiration = [
-      {
-        days = 180
-      }
-    ]
-  }]
-}
