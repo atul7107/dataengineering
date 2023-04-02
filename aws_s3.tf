04b89e2674a921bdd62c7d9d46358f76e3b928da
Atul Yadav <atul.yadav7717@gmail.com>
2023-04-02 17:14:18 +0530

    Update aws_s3.tf

--- aws_s3.tf
+++ aws_s3.tf
@@ -32,13 +32,9 @@ module "dl_s3_raw" {
   bucket                  = var.dl_s3_raw.bucket_name
   count                   = var.dl_s3_raw.create_s3_bucket ? 1 : 0
   force_destroy           = var.dl_s3_raw.force_destroy
-  kms_master_key_arn      = module.dl_kms.kms_arn
   lifecycle_rule          = var.dl_s3_raw.lifecycle_rule
   restrict_public_buckets = var.dl_s3_raw.restrict_public_buckets
-  sse_algorithm           = var.dl_s3_raw.sse_algorithm
-  sse_prevent             = var.dl_s3_raw.sse_prevent
   versioning              = var.dl_s3_raw.versioning
   tags                    = var.tags
 
-  depends_on = [module.dl_kms]
 }
