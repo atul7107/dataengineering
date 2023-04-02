92fe9dd510761879eeb8e59bfeb5ad39ac16eb6b
Atul Yadav <atul.yadav7717@gmail.com>
2023-04-03 01:29:56 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,36 +5,35 @@ module "dl_kms" {
 resource "aws_s3_bucket" "this" {
   # ... other configurations ...
 
-  dynamic "lifecycle_configuration" {
-    for_each = length(var.dl_s3_raw.lifecycle_rule) > 0 ? [1] : []
-    content {
-      rule {
-        dynamic "lifecycle_rule" {
-          for_each = var.dl_s3_raw.lifecycle_rule
-          content {
-            id      = lifecycle_rule.value.id
-            status  = lifecycle_rule.value.enabled ? "Enabled" : "Disabled"
+  lifecycle_configuration {
+    rule {
+      dynamic "lifecycle_rule" {
+        for_each = var.dl_s3_raw.lifecycle_rule
+        content {
+          id      = lifecycle_rule.value.id
+          status  = lifecycle_rule.value.enabled ? "Enabled" : "Disabled"
 
-            dynamic "transition" {
-              for_each = lifecycle_rule.value.transition
-              content {
-                days          = transition.value.days
-                storage_class = transition.value.storage_class
-              }
+          dynamic "transition" {
+            for_each = lifecycle_rule.value.transition
+            content {
+              days          = transition.value.days
+              storage_class = transition.value.storage_class
             }
+          }
 
-            dynamic "expiration" {
-              for_each = lifecycle_rule.value.expiration
-              content {
-                days = expiration.value.days
-              }
+          dynamic "expiration" {
+            for_each = lifecycle_rule.value.expiration
+            content {
+              days = expiration.value.days
             }
           }
         }
       }
     }
   }
+  # ... other configurations ...
 }
+
 # Define the AWS IAM role resource for Glue jobs
 resource "aws_iam_role" "glue_jobs" {
   name = "glue-jobs-role"
