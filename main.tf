6dbacb129a95a9991f4ae5c61a52613cc41e190e
Atul Yadav <atul.yadav7717@gmail.com>
2023-04-03 01:19:16 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,6 +2,36 @@ module "dl_kms" {
   source = "terraform-aws-modules/kms/aws"
 }
 
+resource "aws_s3_bucket" "this" {
+  # ... other configurations ...
+
+  dynamic "lifecycle_configuration_rule" {
+    for_each = var.dl_s3_raw.lifecycle_rule
+    content {
+      id      = lifecycle_configuration_rule.value.id
+      status  = lifecycle_configuration_rule.value.enabled ? "Enabled" : "Disabled"
+
+      dynamic "transition" {
+        for_each = lifecycle_configuration_rule.value.transition
+        content {
+          days          = transition.value.days
+          storage_class = transition.value.storage_class
+        }
+      }
+
+      dynamic "expiration" {
+        for_each = lifecycle_configuration_rule.value.expiration
+        content {
+          days = expiration.value.days
+        }
+      }
+    }
+  }
+
+  # ... other configurations ...
+}
+
+
 # Define the AWS IAM role resource for Glue jobs
 resource "aws_iam_role" "glue_jobs" {
   name = "glue-jobs-role"
