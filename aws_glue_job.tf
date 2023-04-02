7566194d7532b2c785ce340705f15e656efe421f
Atul Yadav <atul.yadav7717@gmail.com>
2023-04-02 21:32:25 +0530

    Update aws_glue_job.tf

--- aws_glue_job.tf
+++ aws_glue_job.tf
@@ -48,7 +48,6 @@ resource "aws_s3_bucket_object" "scripts_raw_to_prepared" {
 resource "aws_glue_security_configuration" "s3_encrypt_decrypt" {
   name                         = "s3_encrypt_decrypt"
   encryption_configuration    = {
-    s3_encryption              = {
       s3_encryption_mode       = "SSE-S3"
       kms_master_key_id        = module.dl_kms.kms_arn
     }
