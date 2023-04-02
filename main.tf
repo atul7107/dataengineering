d17353b96153118695ee6ba514a096c1b3476a3d
Atul Yadav <atul.yadav7717@gmail.com>
2023-04-02 23:17:17 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,21 @@
 module "dl_kms" {
   source = "terraform-aws-modules/kms/aws"
 }
+
+# Define the AWS IAM role resource for Glue jobs
+resource "aws_iam_role" "glue_jobs" {
+  name = "glue-jobs-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "glue.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
