7cd8f46f05c09d29fca993865a0a1330b8592f8c
Atul Yadav <atul.yadav7717@gmail.com>
2023-04-03 01:10:04 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -5,23 +5,6 @@ variable "dl_s3_raw" {
     block_public_acls      = bool
     block_public_policy    = bool
     force_destroy          = bool
-    lifecycle_rule = object({
-      id                     = string
-      prefix                 = string
-      enabled                = bool
-      status                 = string
-      transitions            = list(object({
-        days           = number
-        storage_class  = string
-      }))
-      noncurrent_version_transitions = list(object({
-        days           = number
-        storage_class  = string
-      }))
-      expiration             = list(object({
-        days           = number
-      }))
-    }) # or a more specific type
     restrict_public_buckets = bool
     versioning             = map(string) # Update the versioning type
     create_s3_bucket       = bool # Add the create_s3_bucket attribute
