5ab1b735db90535c8503fa18a2583ccec053df91
Atul Yadav <atul.yadav7717@gmail.com>
2023-04-02 17:23:48 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,8 +1,19 @@
 variable "dl_s3_raw" {
   description = "The information for the S3 raw data bucket"
   type = object({
-    bucket_name = string
-    create_s3_bucket = bool
-    block_public_acls = bool
+    bucket_name            = string
+    block_public_acls      = bool
+    block_public_policy    = bool
+    force_destroy          = bool
+    lifecycle_rule         = any # or a more specific type
+    restrict_public_buckets = bool
+    versioning             = bool
+    # Add any other attributes you need
+variable "tags" {
+  description = "A map of tags to apply to resources"
+  type        = map(string)
+  default     = {}
+}
+
   })
 }
