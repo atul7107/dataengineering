a0caf1bf187a95e8069db94632ab05cd367826d8
Atul Yadav <atul.yadav7717@gmail.com>
2023-04-03 00:06:56 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -70,7 +70,6 @@ variable "dl_s3_prefixes" {
 variable "server_side_encryption_configuration" {
   description = "The server-side encryption configuration for the S3 bucket"
   type        = map(any)
-
   default = {
     rule = {
       apply_server_side_encryption_by_default = {
