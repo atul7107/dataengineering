728949bcb3a87cdfac8fc8332d943c89a4b4981e
Atul Yadav <atul.yadav7717@gmail.com>
2023-04-02 17:29:41 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -8,12 +8,14 @@ variable "dl_s3_raw" {
     lifecycle_rule         = any # or a more specific type
     restrict_public_buckets = bool
     versioning             = bool
-    # Add any other attributes you need
+    
+  })
+}
+
+# Add any other attributes you need
+
 variable "tags" {
   description = "A map of tags to apply to resources"
   type        = map(string)
   default     = {}
-}
-
-  })
 }
