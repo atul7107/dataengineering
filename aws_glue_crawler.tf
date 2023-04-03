05b5c53bc3a4771d68a6956cb004749975420cb5
Atul Yadav <atul.yadav7717@gmail.com>
2023-04-03 12:35:36 +0530

    Update aws_glue_crawler.tf

--- aws_glue_crawler.tf
+++ aws_glue_crawler.tf
@@ -1,23 +1,23 @@
 resource "aws_glue_crawler" "crawler" {
-  
+
   for_each = {
-      for v in local.zone_prefix_map : "${v.zone}:${v.s3_prefix}" => v
+    for v in local.zone_prefix_map : "${v.zone}:${v.s3_prefix}" => v
   }
 
   database_name = aws_glue_catalog_database.data["${each.value.zone}"].name
   name          = "crawler_${each.value.zone}_${each.value.s3_prefix}"
   table_prefix  = "${each.value.s3_prefix}_"
-  role          = aws_iam_role.dl_glue_crawler_role.arn
+  role          = aws_iam_role.dl_glue_crawler_role[count.index].arn
   tags          = var.tags
 
   s3_target {
     path = format(
-            "s3://%s-%s-%s-${each.value.zone}-%s/${each.value.s3_prefix}/",
-            var.tags.region,
-            var.tags.company,
-            var.tags.project,
-            var.tags.environment,
-            )
+      "s3://%s-%s-%s-${each.value.zone}-%s/${each.value.s3_prefix}/",
+      var.tags.region,
+      var.tags.company,
+      var.tags.project,
+      var.tags.environment,
+    )
   }
 
   configuration = <<EOF
@@ -30,17 +30,6 @@ resource "aws_glue_crawler" "crawler" {
   EOF
 }
 
-  #resource "aws_glue_catalog_database" "data" {
-  #for_each = {
-   # for zone in var.dl_zones : zone => {
-    #  name = "${var.dl_catalog_db.name}_${zone}"
-     # description = "${var.dl_catalog_db.description} ${upper(zone)} zone"
-    #}
-  #}
-
-  #name        = each.value.name
-  #description = each.value.description
-#}
 resource "aws_glue_catalog_database" "data" {
   for_each = toset(var.region_availability_zones)
   name = "${var.dl_glue_raw_db}_${each.key}"
@@ -51,7 +40,7 @@ resource "aws_glue_catalog_database" "data" {
     ]
   }
 }
- 
+
 data "aws_iam_role" "existing_role" {
   name  = "dl_glue_crawler_role"
 }
@@ -74,6 +63,7 @@ resource "aws_iam_role" "dl_glue_crawler_role" {
     ]
   })
 }
+
   
 #terraform import aws_iam_role.dl_glue_crawler_role dl_glue_crawler_role  
   
@@ -101,4 +91,4 @@ resource "aws_iam_role" "dl_glue_crawler_role" {
     #]
   #})
 
-}
+#}
