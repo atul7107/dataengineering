73aa8f30b9dea79ce9158eae35fbf600ca9c9a6c
Atul Yadav <atul.yadav7717@gmail.com>
2023-04-03 15:07:55 +0530

    Update aws_s3.tf

--- aws_s3.tf
+++ aws_s3.tf
@@ -24,11 +24,6 @@ provider "aws" {
   region  = "us-east-1"
 }
 
-provider "aws" {
-  alias  = "eu-west-1"
-  region = "eu-west-1"
-}
-
 module "dl_s3_raw" {
   source = "terraform-aws-modules/s3-bucket/aws"
 
